7a1fdfd1a42dd842a074880d1c4a94520c787345
SigningOwl <whddms1208@gmail.com>
2024-06-13 23:29:53 +0900

    Create Models, Superuser and Configure the Django Admin to manage models

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# 질문 모델
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+# 답변 모델
+    # 질문에 대한 답변이 필요하므로 question은 Question 모델에서 외래키로 가져온다.
+    # question은 삭제되면 질문도 함께 삭제되도록 삭제 옵션은 CASCADE로 설정
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
