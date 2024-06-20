cf46e950a9d05aaccd18a50edcf4340a49ecf773
SigningOwl <whddms1208@gmail.com>
2024-06-20 21:58:03 +0900

    Add author field to answer model and question model

--- pybo/models.py
+++ pybo/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # 질문 모델
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -13,6 +15,7 @@ class Question(models.Model):
     # 질문에 대한 답변이 필요하므로 question은 Question 모델에서 외래키로 가져온다.
     # question은 삭제되면 질문도 함께 삭제되도록 삭제 옵션은 CASCADE로 설정
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
